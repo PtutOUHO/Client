096a5d8bef4747bcda45ad7deb33a945c275a8a9
aabouchiche <55394762+aabouchiche@users.noreply.github.com>
2021-03-17 18:02:38 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { NotifierModule } from 'angular-notifier';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -27,7 +26,6 @@ import { environment } from '../environments/environment';
     AngularFireAuthModule,
     AngularFireDatabaseModule,
     AngularFirestoreModule,
-    [NotifierModule]
   ],
   providers: [
     StatusBar,
