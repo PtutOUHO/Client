28553f040511234f02a6a0921d97da942de578a1
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2021-05-11 11:57:48 +0200

    compare quest dates
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- src/app/pages/tabs/quetes/quetes.page.ts
+++ src/app/pages/tabs/quetes/quetes.page.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { AuthenticationService } from "../../../shared/authentication-service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {Quest} from '../../../shared/quest';
+import {AuthenticationService} from '../../../shared/authentication-service';
 
 @Component({
-  selector: "app-quetes",
-  templateUrl: "./quetes.page.html",
-  styleUrls: ["./quetes.page.scss"],
+  selector: 'app-quetes',
+  templateUrl: './quetes.page.html',
+  styleUrls: ['./quetes.page.scss'],
 })
 
 export class QuetesPage implements OnInit {
-  test : any;
   constructor(
+    public router: Router,
+    private quest: Quest,
     public authService: AuthenticationService,
-    public router: Router
-
   ) {}
-    
+  creationDateQuest = this.quest.creation_date;
+  todayDate = Date.now();
+
   questList = [
         {
           quest_name: 'Poulet'
@@ -33,16 +35,49 @@ export class QuetesPage implements OnInit {
           quest_nbRp: '200'
         },
         {
-          quest_distance: 'optionnel'  
+          quest_distance: 'optionnel'
         },
         {
-          quest_time: 'optionnel'  
+          quest_time: 'optionnel'
         },
         {
-          quest_userId: 'optionnel'  
+          quest_userId: 'optionnel'
         }
   ];
 
-  ngOnInit() {
-  }
+    public compareDate(date1: Date, date2: Date): number {
+        const d1 = new Date(date1); const d2 = new Date(date2);
+
+        const same = d1.getTime() === d2.getTime();
+        if (same) { return 0; }
+
+        if (d1 > d2) { return 1; }
+
+        if (d1 < d2) { return -1; }
+    }
+
+    ngOnInit() {
+
+        let uid = JSON.parse(localStorage.getItem('userData')).uid;
+        let collection = this.authService.afStore.collection('Quest', ref => ref.where('quest_userId', '==', uid));
+        let documentList = collection.valueChanges();
+        documentList.forEach(doc => {
+            doc.forEach(quest => {
+                const quete = quest as Quest;
+                console.log(quete.quest_userId);
+            });
+        });
+
+        if (this.compareDate(this.creationDateQuest, this.todayDate) === -1) {
+            this.generateDailyQuest();
+        }
+        this.creationDateQuest.setDate(this.creationDateQuest.getDay() + 7);
+        if (this.compareDate(this.creationDateQuest, this.todayDate) === -1) {
+            this.generateWeeklyQuest();
+        }
+        this.creationDateQuest.setDate(this.creationDateQuest.getDay() + 23);
+        if (this.compareDate(this.creationDateQuest, this.todayDate) === -1) {
+            this.generateMonthlyQuest();
+        }
+    }
 }
